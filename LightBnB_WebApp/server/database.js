de6af47960919092a69e74db4684207d732c321a
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-16 04:14:25 -0400

    catch errors thrown by auth functions

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -32,7 +32,11 @@ const getUserWithEmail = function(email) {
   return pool.query(`
   SELECT * FROM users 
   WHERE email = $1
-  `, [email]).then(res => res.rows[0]);
+  `, [email])
+    .then(res => res.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -48,7 +52,11 @@ const getUserWithId = function(id) {
   return pool.query(`
   SELECT * FROM users 
   WHERE id = $1
-  `, [id]).then(res => res.rows[0]);
+  `, [id])
+    .then(res => res.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -68,7 +76,10 @@ const addUser = function(user) {
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3) RETURNING *
   `, [user.name, user.email, user.password])
-    .then(res => res.rows[0]);
+    .then(res => res.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addUser = addUser;
 
