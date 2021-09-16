09760c7f02b22cdf4d65cb38fbc655e04954db27
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-16 05:19:22 -0400

    allow users to search for properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -125,12 +125,60 @@ exports.getAllReservations = getAllReservations;
 // }
 
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `
+    WHERE city LIKE $${queryParams.length} 
+    `
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += (queryString.length > 150) ? 'AND ' : 'WHERE ';
+    queryString += `
+    properties.owner_id = $${queryParams.length} 
+    `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += (queryString.length > 150) ? 'AND ' : 'WHERE ';
+    queryString += `
+    properties.cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length} 
+    `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += (queryString.length > 150) ? 'AND ' : 'WHERE ';
+    queryString += `
+    property_reviews.rating >= $${queryParams.length} 
+    `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY properties.cost_per_night
+  LIMIT $${queryParams.length}
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows)
+    .catch(error => {
+      console.log(error.message);
+    })
 }
 exports.getAllProperties = getAllProperties;
 
